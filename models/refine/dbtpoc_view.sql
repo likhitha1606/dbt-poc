9ed122374fa561ea3161eec9c697dae82379db6e
likhitha karri <daisy.ptrm@gmail.com>
2024-07-25 06:23:28 +0000

    union qry added

--- models/refine/dbtpoc_view.sql
+++ models/refine/dbtpoc_view.sql
@@ -1,7 +1,8 @@
 {{ config(materialized="view") }}
 
 with
-    source_data as (   select * from {{ref('dbtpoc_limit')}}    )
+    source_data as (   select * from {{ref('dbtpoc_limit')}} union select * from {{ref('dbtpoc_limit_2')}}    
+    )
 select * from  source_data
 
 
