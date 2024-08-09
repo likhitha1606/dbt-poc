5140634fcaabfc7f53d587151352dd3052ac8af0
likhitha karri <daisy.ptrm@gmail.com>
2024-08-09 07:31:09 +0000

    view change

--- models/refine/dbtpoc_view.sql
+++ models/refine/dbtpoc_view.sql
@@ -1,9 +1,12 @@
 {{ config(materialized="view") }}
 
 with
-    source_data as (   select * from {{ref('dbtpoc_limit')}} union select * from {{ref('dbtpoc_limit_2')}}    
+    source_data as (
+        select *
+        from {{ ref("dbtpoc_limit") }}
+        union
+        select *
+        from {{ ref("dbtpoc_limit_2") }}
     )
-select * from  source_data
-
-
-
+select *
+from source_data
